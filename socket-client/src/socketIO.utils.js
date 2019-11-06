a360e4ce6fb982e4e4829995c9d03c210ac41c44
liu-austin <austinliu279@gmail.com>
2019-11-05 18:04:00 -0800

    fixes ports

--- socket-client/src/socketIO.utils.js
+++ socket-client/src/socketIO.utils.js
@@ -2,7 +2,8 @@
 import socketIOClient from 'socket.io-client';
 
 const state = {
-    endpoint: 'http://localhost:4001'
+    endpoint: 'https://reactsocketiopresentation.herokuapp.com/',
+    // sendpoint: 'http://localhost:4001'
 };
 
 export const socket = socketIOClient(state.endpoint);
