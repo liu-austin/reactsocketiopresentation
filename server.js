fab6f1b1e48b71111afc41744a8686400f9dc5db
liu-austin <austinliu279@gmail.com>
2019-11-05 18:53:44 -0800

    serves index.html

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const socketIO = require('socket.io');
 const port = process.env.PORT || 4001;
 
 const app = express();
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('socket-client/build'));
 
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'socket-client', 'build', 'index.html'));
+  });
+}
 // our server instance
 const server = http.createServer(app);
 
